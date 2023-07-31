c99c3f87ac5033830e436c6a81f60afac93ed74f
kevinseestone <kevinseestone@gmail.com>
2023-07-31 10:05:17 -0700

    added AppBar with no functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
+import ButtonAppBar from './ButtonAppBar';
+
 
 
 const App = () => {
 
   return (
     <div className="App">
-
+      <ButtonAppBar />
     </div>
+
   );
 }
 
