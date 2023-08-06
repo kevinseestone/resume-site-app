c3eed4ee3a4c33e45e3975e18b0840ea32b17914
Kevin C Stone <140640175+kevinseestone@users.noreply.github.com>
2023-08-06 23:04:09 +0000

    created outline for resume page

--- src/MainCard.js
+++ src/MainCard.js
@@ -1,22 +1,34 @@
 import * as React from 'react';
+import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import { Padding } from '@mui/icons-material';
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
 
 const SimplePaper = () => {
     return (
-        <Box
-            sx={{
-                display: 'flex',
-                flexWrap: 'wrap',
-                '& > :not(style)': {
-                    m: 1,
-                    width: 128,
-                    height: 128,
-                },
-                }}
-        >
-            <Paper elevation={3} />
-        </Box>
+    <Box sx={{ 
+        flexGrow: 1,
+        paddingTop: 15,
+        paddingLeft: 10
+        }}>
+      <Grid container spacing={2}>
+        <Grid item md={4}>
+          <Item>Kevin Stone</Item>
+        </Grid>
+        <Grid item md={6}>
+          <Item>Resume</Item>
+        </Grid>
+      </Grid>
+    </Box>
     );
 }
 
